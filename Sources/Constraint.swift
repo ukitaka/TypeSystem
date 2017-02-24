3750bb0410190a5ed127e1da65292da56a3f3d3c
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-24 22:38:07 +0900

    Add `unify`

--- Sources/Constraint.swift
+++ Sources/Constraint.swift
@@ -53,3 +53,38 @@ struct ConstraintSet {
         self.equations = equations
     }
 }
+
+func unify(_ equations: [Equation]) -> Substitution {
+    guard let head = equations.first else {
+        return Substitution()
+    }
+
+    let (S, T) = (head.left, head.right)
+
+    switch (S, T) {
+    case (.typeVar(let a), .typeVar(let b)) where a == b:
+        return unify(Array(equations.dropFirst()))
+
+    case (.type(let a), .type(let b)) where a == b:
+        return unify(Array(equations.dropFirst()))
+
+    case (.typeVar, _) where !T.typeVars.contains(S):
+        let sub = Substitution(typeVar: S, type: T)
+        let c = ConstraintSet(equations: Array(equations.dropFirst()))
+        return unify(sub.apply(constraintSet: c).equations) • sub
+
+    case (_, .typeVar) where !S.typeVars.contains(T):
+        let sub = Substitution(typeVar: T, type: S)
+        let c = ConstraintSet(equations: Array(equations.dropFirst()))
+        return unify(sub.apply(constraintSet: c).equations) • sub
+
+    case (.arrow(let s1, let s2), .arrow(let t1, let t2)):
+        var e = Array(equations.dropLast())
+        e.append(Equation(left: s1, right: t1))
+        e.append(Equation(left: s2, right: t2))
+        return unify(e)
+
+    default:
+        fatalError("Unification error. \(head)")
+    }
+}
